1828933cc3c447a950edecc5a44262ec7d7bdc25
dean.edwards <dean.edwards@3dd6368b-0143-0410-a5fb-875d7f22da6c>
2010-02-20 16:41:02 +0000

    Version 2.1 (beta1)

--- test/ie7-demo.js
+++ test/ie7-demo.js
@@ -1,11 +1,11 @@
 document.write("<style>"+
-((/^ms/.test(document.documentElement.uniqueID)&&!/MSIE [89]/.test(navigator.appVersion))?"":"#ie7_demo button{display:none;}")+
+(('IE7'in window&&IE7.compat>=IE7.appVersion)?"":"#ie7_demo button{display:none;}")+
 "#ie7_demo{position:relative;background:"+
-((/^ms/.test(document.documentElement.uniqueID)&&!/MSIE [89]/.test(navigator.appVersion)&&location.search!="?ie7_off")?"yellow":"#ccc")+
+(('IE7'in window&&IE7.compat>=IE7.appVersion&&location.search!="?ie7_off")?"yellow":"#ccc")+
 ";color:black;padding:4px 0;margin:0;text-indent:8px;"+
 "left:0;top:0;letter-spacing:normal;text-align:left;height:auto;width:200px;border:none;"+
 "font:bold 32px/36px Verdana,Arial,Helvetica,sans-serif;text-transform:none;z-index:99;}"+
-"#ie7_demo button{margin-left:20px;padding:0;line-height:16px;vertical-align:middle;text-indent:0px;}</style>"+
+"#ie7_demo button{margin:6px 0 0 20px;line-height:normal;vertical-align:top;text-indent:0px;}</style>"+
 "<h1 id='ie7_demo'>IE7&nbsp;<button type=button onclick=\"location.replace(location.pathname+'"+
 (location.search=="?ie7_off"?"":"?ie7_off")+"')\">"+
 (location.search=="?ie7_off"?"Apply":"Remove")+"</button></h1>");
